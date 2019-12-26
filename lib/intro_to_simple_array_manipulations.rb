e20ba05e9e86ea82a0c63c4a6d7b7713926649f6
K e n V <kenneth_valdivia@yahoo.com>
2019-12-26 02:55:32 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,32 @@
+def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+  artists.push(next_artist)
+end
+def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+  artists.unshift
+end
+def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+  artists.pop
+end
+def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+  artists.pop(2)
+end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    artist.unshift
+  end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    artists.concat
+  end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    artists.insert 
+  end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    artists.uniq 
+  end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    artists.flatten
+  end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    artists.delete
+  end
+  def artists = ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"]
+    delete_at (0)
